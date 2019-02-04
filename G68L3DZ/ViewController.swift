e54a6de548bc49017c5a3e6a597cbc895de5e62c
Michael Koka <tikki-mikk@yandex.ru>
2019-02-04 18:50:35 +0200

    Dobavil reshenie dlya 1.3 i 1.4

--- G68L3DZ/ViewController.swift
+++ G68L3DZ/ViewController.swift
@@ -12,16 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let firstNumber = Int(Int.random(in: 1...10))
-        let secondNumber = Int(Int.random(in: 1...10))
+        
+        
+        //Zadanie 1.0
+        let firstNumber = Int.random(in: 1...10)
+        let secondNumber = Int.random(in: 1...10)
         print(firstNumber)
         print(secondNumber)
         print("Bolshee iz dvuh chisel: ",maxNumber(firstNumber: firstNumber, secondNumber: secondNumber))
-       var r = Int.random(in: 1...10)
+        //Zadanie 1.1
+        let r = Int.random(in: 1...10)
         print("Chislo dlya vozvedeniya v kvadrat i kub :", r)
         print("Kvadrat chisla :",squareOfNumber(a: r))
         print("Cube chisla :", cubeOfNumber(a: r))
+        
+        //Zadanie 1.3
+        let dNumber = Int.random(in: 2...20)
+        print("Chislo dlya kotorogo nuzhno naiti deliteli ", dNumber)
+        delitellOtChisla(a: dNumber)
+        
+        //Zadanie 1.4
+        let nNumber = Int.random(in: 2...1000)
+        print("Uznaem yavlyaetsa li chislo ", nNumber, "sovershennim")
+        delitelOtChislaVerPerfect(b: nNumber)
+        
+        
+    
         
     }
 }
@@ -45,5 +61,28 @@ func squareOfNumber (a:Int)->Int {
 func cubeOfNumber (a:Int)->Int {
     let cube = (a*a*a)
     return cube
+}
+
+func delitellOtChisla (a:Int) {
+    for i in 1...a{
+        if a%i == 0 {
+            print("Delitel eto ", i)
+        }
+        
+}
+}
+func delitelOtChislaVerPerfect (b:Int) {
+    var sumDelitelei:Int = 0
+    for i in 1...b{
+        if b%i == 0 {
+            print("Deliteli chisla", i)
+            sumDelitelei = i+sumDelitelei
+}
+}
+    if sumDelitelei == b{
+        print("Chislo :", b, "sovershennoe")
+    } else {
+        print("Chislo :", b, "ne sovershennoe")
     
+}
 }
